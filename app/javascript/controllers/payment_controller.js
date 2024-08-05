47d7a05f3f214b37fac926632759810d4b159d31
borgar90 <borgar90@gmail.com>
2024-08-05 16:51:00 +0200

    order will now store, and successmsg will be shown!

--- app/javascript/controllers/payment_controller.js
+++ app/javascript/controllers/payment_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus";
+
 // Connects to data-controller="payment"
 export default class extends Controller {
   static targets = ["selection", "additionalFields"];
